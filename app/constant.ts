0f53b677636ae62f1450723177320927a0c7ec94
zby <909908771@qq.com>
2024-12-04 14:21:14 +0800

    feat: 更新Claude模型

--- app/constant.ts
+++ app/constant.ts
@@ -258,8 +258,13 @@ const anthropicModels = [
   "claude-2.1",
   "claude-3-sonnet-20240229",
   "claude-3-opus-20240229",
+  "claude-3-opus-latest",
   "claude-3-haiku-20240307",
+  "claude-3-5-haiku-20241022",
+  "claude-3-5-haiku-latest",
   "claude-3-5-sonnet-20240620",
+  "claude-3-5-sonnet-20241022",
+  "claude-3-5-sonnet-latest",
 ];
 
 const baiduModels = [
